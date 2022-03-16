017678d875b6eec272cb8345e0830845ab53f925
triger99 <51343541+triger99@users.noreply.github.com>
2022-03-17 00:36:51 +0900

    Hee (#44)
    * Add files via upload
    
    * Create backjon.md
    
    * Delete backjon.md
    
    * Create backjon.md
    
    * Delete docs directory
    
    * Create test
    
    * Add files via upload
    
    * Delete backjon1260(dfs,bf)s.py
    
    * Delete backjon2178(bfs).py
    
    * Delete bakjon4963.py
    
    * Delete kmp.py
    
    * Add files via upload
    
    * Add files via upload
    
    백준 나이트의 이동
    최단 경로를 탐색해야 하기 때문에 bfs algorithm 사용
    https://www.acmicpc.net/problem/7562
    
    * Add files via upload
    
    conneted graph
    dfs  사용
    
    * Add files via upload
    
    dfs,bfs 알고리즘
    
    * Add files via upload
    
    백준 1012번 https://www.acmicpc.net/problem/1012
    dfs,bfs 알고리즘
    
    * Add files via upload
    
    https://www.acmicpc.net/problem/7576

---
+++ baekjoon/baekjon7576.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys
+## 최소 일 수를 구해야 함으로 bfs 알고리즘을 사용해야 한다.
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = dx[i] + x, dy[i] + y
+            if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 0:
+                matrix[nx][ny] = matrix[x][y] + 1
+                queue.append([nx, ny])
+
+m, n = map(int, sys.stdin.readline().split())
+matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+queue = deque([])
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+res = 0
+
+for i in range(n):
+    for j in range(m):
+        if matrix[i][j] == 1:
+            queue.append([i, j])
+
+
+bfs()
+for i in matrix:
+    for j in i:
+        if j == 0:
+            print(-1)
+            exit(0)
+    res = max(res, max(i))
+print(res - 1)
